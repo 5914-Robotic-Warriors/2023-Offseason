862ce16eeb625e222804aeb0a6700c4e21e297a3
5914 Programming <5914programming@gmail.com>
2023-06-27 20:21:12 -0500

    testing vega code

--- NewVegaCode/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ NewVegaCode/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,53 +1,18 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveSubsystem extends SubsystemBase {
 
-  private final CANSparkMax frontLeftDriveMotor = new CANSparkMax(1, MotorType.kBrushless);
-  
-  /** Creates a new ExampleSubsystem. */
-  public DriveSubsystem() {}
+    private final CANSparkMax frontLeftDriveMotor = new CANSparkMax(1, MotorType.kBrushless);
+    private final CANSparkMax frontRightDriveMotor = new CANSparkMax(2, MotorType.kBrushless);
+    private final CANSparkMax backLeftDriveMotor = new CANSparkMax(3, MotorType.kBrushless);
+    private final CANSparkMax backRightDriveMotor = new CANSparkMax(4, MotorType.kBrushless);
 
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-  public CommandBase exampleMethodCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-        });
-  }
+    public DriveSubsystem() {
+    }
 
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
-  }
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
-
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
 }
